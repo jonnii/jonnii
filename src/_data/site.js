615ba352960bb9f7f6cbfd957802ffccad956c64
jonnii <me@jonnii.com>
2020-11-22 21:29:08 -0500

    add data

---
+++ src/_data/site.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "jonnii.com",
+  description:
+    "jonnii.com",
+  url: "https://jonnii.com",
+  author: "jonnii",
+  meta_data: {
+    twitter: "@jonnii",
+    default_social_image: "/static/default_social_image.jpg",
+  },
+  env: process.env.ELEVENTY_ENV === "production",
+}
